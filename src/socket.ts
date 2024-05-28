112bf2db747d76d474bfb35d3b7ce227eb02c4e0
astro348 <astrodaniel348@gmail.com>
2024-05-28 07:32:13 +0300

    commit 1

--- src/socket.ts
+++
@@ -1,7 +0,0 @@
-import { io } from "socket.io-client";
-
-// "undefined" means the URL will be computed from the `window.location` object
-
-const URL = process.env.REACT_APP_SERVER_BASE_URL!;
-
-export const socket = io(URL);
